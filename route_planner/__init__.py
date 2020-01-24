d329b71e5984419bb6c596bf65bfbaba623a9d6c
Aimsucks <jakeschlink@gmail.com>
2020-01-24 16:25:36 -0500

    Make sure to uncomment this stuff when it gets deployed.

--- route_planner/__init__.py
+++ route_planner/__init__.py
@@ -1,1 +1,4 @@
+# from django.conf import settings
+# if not settings.DEBUG:
+#     default_app_config = 'route_planner.apps.RoutePlannerConfig'
 default_app_config = 'route_planner.apps.RoutePlannerConfig'
