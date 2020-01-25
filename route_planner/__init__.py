3195723075bc02a99a58a047681d8186da507df3
Aimsucks <jakeschlink@gmail.com>
2020-01-25 18:29:07 -0500

    Almost forgot to uncomment this!

--- route_planner/__init__.py
+++ route_planner/__init__.py
@@ -1,4 +1,3 @@
-# from django.conf import settings
-# if not settings.DEBUG:
-#     default_app_config = 'route_planner.apps.RoutePlannerConfig'
-default_app_config = 'route_planner.apps.RoutePlannerConfig'
+from django.conf import settings
+if not settings.DEBUG:
+    default_app_config = 'route_planner.apps.RoutePlannerConfig'
