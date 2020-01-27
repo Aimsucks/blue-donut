20df37c62abcd5574b22660e048e34c76d022636
Aimsucks <jakeschlink@gmail.com>
2020-01-26 19:01:39 -0500

    Added scope selection to login.

--- eve_auth/views.py
+++ eve_auth/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import authenticate, login
 from django.views.generic.base import View
-from django.shortcuts import redirect
+from django.shortcuts import render, redirect
 
 from uuid import uuid4
 
@@ -9,13 +9,19 @@ from eve_esi import ESI
 
 class LoginView(View):
     def get(self, request):
+        return render(request, "eve_auth/scope_selection.html")
 
+    def post(self, request):
         scopes = [
             'esi-location.read_location.v1',
             'esi-ui.write_waypoint.v1',
-            'esi-search.search_structures.v1',
-            'esi-universe.read_structures.v1'
         ]
+
+        if "approve" in request.POST:
+            scopes.extend([
+                'esi-search.search_structures.v1',
+                'esi-universe.read_structures.v1'
+            ])
 
         return redirect(
             ESI.get_security().get_auth_uri(state=str(uuid4()), scopes=scopes)
