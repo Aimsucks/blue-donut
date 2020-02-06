fb18c220a7af6f113415f52d01e0144b02b10a8f
Aimsucks <contact@aimsucks.space>
2020-02-05 23:08:48 -0500

    Small change since I didn't work on it much tonight.

--- blue_donut/settings.py
+++ blue_donut/settings.py
@@ -64,7 +64,10 @@ LOGOUT_REDIRECT_URL = '/'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
-    ]
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
 }
 
 ############################
