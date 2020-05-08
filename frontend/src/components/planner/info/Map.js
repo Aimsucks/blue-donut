e80536950209b251c1c7e209836798238f3b56de
Aimsucks <contact@aimsucks.space>
2020-05-08 13:19:30 -0400

    Moved map to dotlan

--- frontend/src/components/planner/info/Map.js
+++ frontend/src/components/planner/info/Map.js
@@ -1,24 +1,24 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 
 export class Map extends Component {
-    render() {
-        return (
-            <>
-                <div className="text-center">
-                    <object
-                        id="map"
-                        className="rounded"
-                        width="65%"
-                        data={
-                            "https://bluedonut.space/svg/Universe.svg?&path=" +
+  render () {
+    return (
+      <>
+        <div className='text-center'>
+          <object
+            id='map'
+            className='rounded'
+            width='65%'
+            data={
+              'http://evemaps.dotlan.net/svg/Universe.svg?&path=' +
                             this.props.route.dotlan_path
-                        }
-                        type="image/svg+xml"
-                    ></object>
-                </div>
-            </>
-        );
-    }
+            }
+            type='image/svg+xml'
+          />
+        </div>
+      </>
+    )
+  }
 }
 
-export default Map;
+export default Map
